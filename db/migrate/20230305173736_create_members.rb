350cb1f18764651eccab2cd876c76030932e9755
vivianne <viviannjoroge91@gmail.com>
2023-03-05 20:41:31 +0300

    update the migrations

--- db/migrate/20230305173736_create_members.rb
+++ db/migrate/20230305173736_create_members.rb
@@ -1,4 +1,11 @@
 class CreateMembers < ActiveRecord::Migration[6.1]
   def change
+    create_table :members do |t|
+      t.string :name
+      t.text :email
+      t.integer :user_id
+      t.integer :project_id
+      t.timestamps
+    end
   end
 end
--- db/migrate/20230305173744_create_projects.rb
+++ db/migrate/20230305173744_create_projects.rb
@@ -1,4 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[6.1]
   def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :title
+      t.string :description 
+
+      t.timestamps
+    end
   end
 end
